f71019806f493dbb9173d86f288513bb8fd6d20d
Gabriel Tyler <gabe@Gabriels-MacBook.local>
2023-08-10 10:32:47 -0500

    start challenge 05

---
+++ 05-LinearAlgebra/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* using arrays, write functions that do...: 
+ *  - vector-to-vector or matrix-vector products 
+ *  - gaussian elimination 
+ *  - iterative matrix inversion */
+
+int main(void) {
+}
